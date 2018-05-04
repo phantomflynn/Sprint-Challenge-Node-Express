4a55c31146aa0ff91770bb1b2a281f88eebcfcb1
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-04 12:22:56 -0400

    created react app , added cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const helmet = require('helmet');
 const actionRoutes = require('./resources/actions');
 const projectRoutes = require('./resources/projects');
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 server.use(helmet());
 
 server.use('/actions', actionRoutes);
