d9954d7715978cfcd235fefc9ff9c9eba902ea1b
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-04 12:45:09 -0400

    rendering project information in react app

--- server-app/src/App.js
+++ server-app/src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from'axios';
 
 class App extends Component {
+  state = {
+    projects: []
+  };
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/projects')
+      .then(response => this.setState({ projects: response.data }))
+      .catch(error => console.log('encountered an error fetching projects'))
+  }
+  
   render() {
     return (
       <div className="App">
@@ -10,9 +21,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.projects.map(project => (
+          <div key={project.id}>
+            <ul>
+              <li>{project.name}</li>
+              <li>{project.description}</li>
+              <li>{project.completed}</li>
+            </ul>
+          </div>
+        ))}
       </div>
     );
   }
