0eb01b178e6df16b82b652783de6207972af558a
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-04 11:49:29 -0400

    add action with project id stipulations

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const actionRoutes = require('./resources/actions');
+const projectRoutes = require('./resources/projects');
 const server = express();
 
 server.use(express.json());
@@ -9,5 +10,6 @@ server.use(helmet());
 server.get('/', (req, res) => res.send('server is working'));
 
 server.use('/actions', actionRoutes);
+server.use('/projects', projectRoutes);
 
 server.listen(5000);
